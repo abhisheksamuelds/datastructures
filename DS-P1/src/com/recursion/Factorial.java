ddeb92ee85a85d3ea65deff64467092aff4e593f
Abhishek <Abhishek@mediacorp.com.sg>
2020-04-19 00:42:41 +0800

    Addtion of Recursion - Factorial

---
+++ DS-P1/src/com/recursion/Factorial.java
@@ -0,0 +1,13 @@
+package com.recursion;
+
+public class Factorial {
+	
+	//Time Complexity O(n)
+	public int fact(int n){
+		if(n == 0 || n == 1){
+			return 1;
+		}
+		return n * fact(n-1);
+	}
+
+}
---
+++ DS-P1/src/com/recursion/FactorialMain.java
@@ -0,0 +1,11 @@
+package com.recursion;
+
+public class FactorialMain {
+
+	public static void main(String[] args) {
+		Factorial fact = new Factorial();
+		System.out.println(fact.fact(1));
+
+	}
+
+}
